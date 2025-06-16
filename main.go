8c6a5e70ca7802741b9c6cd8eaafcc364fa49e10
Adam Reuben <adamreuben@bongocloud.co.tz>
2025-06-16 19:54:05 +0300

    changes

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/adamwreuben/twiggasdk/twigga"
+)
+
 func main() {
-	// twiggaClient := twigga.NewTwiggaClient("", "Authentication")
+	twiggaClient, err := twigga.NewTwiggaClient("./twigga/bongo.json")
+	if err != nil {
+		fmt.Println("error**: ", err.Error())
+		return
+	}
 
-	// dataToFilter := map[string]any{
-	// 	"email": "adamreuben66@gmail.com",
-	// }
+	dataToFilter := map[string]any{
+		"appSecret": "twigga",
+	}
 
-	// resp, err := twiggaClient.QueryDocuments(context.Background(), "Users", dataToFilter)
-	// if err != nil {
-	// 	fmt.Println("Error*: ", err.Error())
-	// 	return
-	// }
+	found, err := twiggaClient.DocumentExists(context.Background(), "Applications", dataToFilter)
+	if err != nil {
+		fmt.Println("Error*: ", err.Error())
+		return
+	}
 
-	// fmt.Println("Response: ", resp)
+	fmt.Println("FOUND: ", found)
+
 }
